ba826c5b2b794bf42f262f3cde0230c4a5255eb0
Luke Perez <lukegothic@gmail.com>
2023-03-07 19:51:19 +0100

    hack para inventariar por coleccion

--- src/ui/components/Album.jsx
+++ src/ui/components/Album.jsx
@@ -21,9 +21,19 @@ const Album = ({ collection, filter, type = AlbumType.SELL }) => {
         window.scrollTo(0, 0);
     }, [page]);
     useUpdateEffect(() => {
-        console.log("chambia filtro de ", type);
-        setFilteredCollection(collection.filter(card => !filter || (filter.toLowerCase() && card.name.toLowerCase().indexOf(filter) !== -1)));
-        setPage(1);
+        // TODO: Super hacky para inventariar sets, hacerlo bien
+        if (filter) {
+            const lowerFilter = filter.toLowerCase();
+            if (filter.indexOf("s:") !== -1) {
+                const set_code = filter.replace("s:", "");
+                setFilteredCollection(collection.filter(card => card.set === set_code));
+            } else {
+                setFilteredCollection(collection.filter(card => card.name.toLowerCase().indexOf(lowerFilter) !== -1));
+            }
+            setPage(1);
+        } else {
+            setFilteredCollection(collection);
+        }
     }, [filter]);  // BUG: si dependo de colleciton hace pintados infinitos en SellPage pero necesito escucharla...
     return <>
         <div className={`album album-${type}`}>
